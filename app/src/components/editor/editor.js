ec8baaa5674ef98d98d109d5efceffb4731c09d6
Andrey <andrey.kallko@gmail.com>
2019-12-31 17:17:05 +0300

    dd

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,38 +1,87 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./../../helpers/iframeLoader.js";
 
 export default class Editor extends Component {
   constructor() {
     super();
 
+    this.currentPage = "index.html";
+
     this.state = {
       pageList: [],
       newPageName: ""
     };
+    this.createNewPage = this.createNewPage.bind(this);
   }
 
   componentDidMount() {
+    this.init(this.currentPage);
+  }
+
+  init(page) {
+    this.iframe = document.querySelector("iframe");
+    this.open(page);
     this.loadPageList();
+  }
+
+  open(page) {
+    this.currentPage = `../${page}`;
+    this.iframe.load(this.currentPage, () => {
+      console.log(this.currentPage);
+    });
   }
 
   loadPageList() {
     axios.get("./api").then(res => this.setState({ pageList: res.data }));
   }
 
-  createNewPage() {}
+  createNewPage() {
+    axios
+      .post("./api/createNewPage.php", { name: this.state.newPageName })
+      .then(res => {
+        console.log(res);
+        this.setState({ newPageName: "" });
+        this.loadPageList();
+      })
+      .catch(() => alert("Такая страница уже существует"));
+  }
+
+  deletePage(page) {
+    axios
+      .post("./api/deletePage.php", { name: page })
+      .then(res => {
+        console.log(res);
+        this.loadPageList();
+      })
+      .catch(() => alert("Что-то пошло не так"));
+  }
 
   render() {
-    const { pageList } = this.state;
-    const pages = pageList.map((page, i) => {
-      return <h1 key={i}>{page}</h1>;
-    });
+    // console.log("render");
+    // const { pageList } = this.state;
+    // const pages = pageList.map((page, i) => {
+    //   return (
+    //     <h1 key={i}>
+    //       {page}
+    //       <a href="#" onClick={() => this.deletePage(page)}>
+    //         (x)
+    //       </a>
+    //     </h1>
+    //   );
+    // });
 
     return (
-      <>
-        <input type="text" />
-        <button>Создать страницу</button>
-        {pages}
-      </>
+      <iframe src={this.currentPage} frameBorder="0"></iframe>
+      // <>
+      //   <input
+      //     type="text"
+      //     value={this.state.newPageName}
+      //     onChange={event => this.setState({ newPageName: event.target.value })}
+      //   />
+      //   <button onClick={this.createNewPage}>Создать страницу</button>
+      //   {pages}
+      // </>
     );
   }
 }
